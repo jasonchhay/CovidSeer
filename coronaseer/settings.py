f60cd415ba36efb9006fd6d024a49d3b37fee481
Jason Chhay <jqc6195@lrs-dwang.ist.psu.edu>
2020-03-22 20:05:24 -0400

    Updated permission settings for database

--- coronaseer/settings.py
+++ coronaseer/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'coronaseer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'coronaseer/coronaseer/../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
