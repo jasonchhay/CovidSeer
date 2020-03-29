4457361a98c8db09ca00abcdb1c7717e15466948
jasonchhay <jqc6195@lws-giles01.ist.psu.edu>
2020-03-29 12:21:53 -0400

    Staged changes for production

--- covidseer/wsgi.py
+++ covidseer/wsgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+#Adam addition
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+#End Adam addition
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covidseer.settings')
 
 application = get_wsgi_application()
+
+#Adam addition test
+#sys.path.append('/data/websites/covidseer/covidseer')
+#sys.path.append('/data/websites/covidseer')
