d1daaa4247aba229bd63c73b43fe1e204b938fa3
jasonchhay <jqc6195@lws-giles01.ist.psu.edu>
2020-03-29 12:29:54 -0400

    Fixed issue with static files directory including static root

--- covidseer/settings.py
+++ covidseer/settings.py
@@ -126,6 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "seer/static")
 ]
