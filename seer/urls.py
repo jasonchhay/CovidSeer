0125e91bbe203a5dc2f4452f7fc7b467a31cb8e8
jasonchhay <jasonchhay@gmail.com>
2020-03-29 22:25:59 -0400

    Updated documents page, added json view, updated results page

--- seer/urls.py
+++ seer/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('search', views.Query, name='search'),
     path('doc/id/<document_id>/', views.Document, name='document'),
+    path('doc/json/id/<document_id>.json', views.DocumentJson, name='document_json')
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
