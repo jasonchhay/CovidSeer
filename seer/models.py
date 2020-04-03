a591013f5970c3da2975f76ac3539f716f65212a
jasonchhay <jasonchhay@gmail.com>
2020-04-02 23:01:03 -0400

    Added author metadata, updated site metadata

--- seer/models.py
+++ seer/models.py
@@ -5,10 +5,11 @@ from django.db import models
 
 class SearchResult:
     def __init__(self, resultid, content="No content found", fileurl="No URL found", title="No title found",
-                 description="No description found", affiliation="No location found"):
+                 authors="No authors available", description="No description found", affiliation="No location found"):
         self.resultid = resultid
         self.content = content
         self.fileurl = fileurl
         self.title = title
+        self.authors = authors
         self.description = description
         self.affiliation = affiliation
